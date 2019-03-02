843c45ddebb797614c942149c98afc4829590776
seebk <mail@sebastiankraft.net>
2019-03-02 20:04:52 +0100

    Improve preparation script for the Windows installer, download dependencies to compile glib schemas

--- tools/innosetup/prepare.ps1
+++ tools/innosetup/prepare.ps1
@@ -1,17 +1,19 @@
 
-# download ressources
+# Download ressources from MSYS2
 $msys2_repo= "http://repo.msys2.org/mingw/x86_64/"
 
 $client = new-object System.Net.WebClient
-$zipExe = join-path ${env:ProgramFiles(x86)} '7-zip\7z.exe'
+$zipExe = join-path ${env:ProgramFiles} '7-zip\7z.exe'
+$pwd = Get-Location
 
-function get_msys2_package($pkg){
-
+function get_msys2_package($pkg){	
     If (-Not (Test-Path "$pkg")){
-        $client.DownloadFile("$msys2_repo/$pkg", $pkg)
+    	Write-Host "Downloading $pkg ..."
+        $client.DownloadFile("$msys2_repo/$pkg", (Join-Path $pwd $pkg))
     }
-    &$zipExe x $pkg -y
-    &$zipExe x $pkg.Substring(0,$pkg.Length-3) -y -opygobject
+    Write-Host "Extracting $pkg ..."
+    &$zipExe x $pkg -y | Out-Null
+    &$zipExe x $pkg.Substring(0,$pkg.Length-3) -y -opygobject | Out-Null
     Remove-Item $pkg.Substring(0,$pkg.Length-3)
 }
 
@@ -26,8 +28,22 @@ get_msys2_package("mingw-w64-x86_64-python2-cairo-1.13.3-2-any.pkg.tar.xz")
 get_msys2_package("mingw-w64-x86_64-python2-gobject-3.24.1-1-any.pkg.tar.xz")
 get_msys2_package("mingw-w64-x86_64-adwaita-icon-theme-3.24.0-1-any.pkg.tar.xz")
 
+# Dependencies for glib-compile-schemas.exe
+get_msys2_package("mingw-w64-x86_64-glib2-2.52.0-1-any.pkg.tar.xz")
+get_msys2_package("mingw-w64-x86_64-pcre-8.41-1-any.pkg.tar.xz")
+get_msys2_package("mingw-w64-x86_64-gettext-0.19.8.1-3-any.pkg.tar.xz")
+get_msys2_package("mingw-w64-x86_64-libwinpthread-git-5.0.0.4833.f057c525-1-any.pkg.tar.xz")
+get_msys2_package("mingw-w64-x86_64-libiconv-1.15-1-any.pkg.tar.xz")
+
+
+# Compile gschemas
+Write-Host "Compiling glib schemas ..."
+&"pygobject\mingw64\bin\glib-compile-schemas.exe" pygobject\mingw64\share\glib-2.0\schemas
+
 # pdf2svg
 If (-Not (Test-Path "pdf2svg.zip")){
+	Write-Host "Downloading pdf2svg ..."
     $client.DownloadFile("https://github.com/jalios/pdf2svg-windows/archive/master.zip", "pdf2svg.zip")
 }
-Expand-Archive pdf2svg.zip -dest .
+Write-Host "Extracting pdf2svg ..."
+Expand-Archive pdf2svg.zip -Force -dest .
